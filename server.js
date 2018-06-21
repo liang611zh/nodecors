6b1f680d0e5b502969a1a49bee0587752b02093b
victor zhao <zhaoliangbz@gmail.com>
2018-06-21 15:27:25 -0700

    push to github repo

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const app = express()
+
 // Enable CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -9,21 +10,14 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
     next();
  });
-
+//solve the bodyparser stream issues
  app.use(bodyParser.urlencoded({extended: true}));
  app.use(bodyParser.json({limit:'50mb'}));
 
  app.use('/', function(req, res) {  
-    var url = req.url.replace('/?url=','');
+    var url = req.url.replace('/?target=','');
     req.pipe(request(url)).pipe(res);
-
   });
 
-  
-//  const url='http://www.pjliaing.com/json/list.json'
-  // POST method route
-app.post('/post', function (req, res) {
-    res.send('POST request to the homepage')
-  })
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Exampl e app listening on port 3000!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
