05fe7014fec886831750a9682fdd32a3aca345e8
Victor Zhao <zhaoliangbz@gmail.com>
2018-10-12 12:03:15 -0700

    Update server.js
    add method

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 // Enable CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
     next();
  });
